3e4263e20409d7161c6862f17b840f9d2ec6ad3d
brettgrigsby <gbrett@gmail.com>
2020-05-04 16:53:30 -0500

    update to use ids

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,38 @@
 import { useState } from 'react'
 import { Hand } from 'react-handy'
 
-const CARDS = [
-  { imgSrc: './atst.jpg' },
-  { cardText: 'Card with text supplied instead of an image' },
-  { imgSrc: './f11d.jpg' },
-  {},
-  { imgSrc: './grievous.jpg' },
-  { imgSrc: './phasma.jpg' },
-  { imgSrc: './presence.jpg' },
-  { imgSrc: './tfighter.jpg' },
-  { imgSrc: './trooper.jpg' },
-  { imgSrc: './veers.jpg' },
+const STARTING_CARDS = [
+  { id: 'starting', imgSrc: './atst.jpg' },
+  { id: 'cards', cardText: 'Card with text supplied instead of an image' },
+  { id: 'are', imgSrc: './f11d.jpg' },
+  { id: 'cool' },
+]
+
+const IMG_URLS = [
+  './atst.jpg',
+  './f11d.jpg',
+  './grievous.jpg',
+  './phasma.jpg',
+  './presence.jpg',
+  './tfighter.jpg',
+  './trooper.jpg',
+  './veers.jpg'
 ]
 
 export default function Home() {
-  const [cards, setCards] = useState(CARDS.slice(0,4))
+  const [cards, setCards] = useState(STARTING_CARDS)
+  const [nextId, setNextId] = useState(0)
 
-  const addCard = () => setCards([ ...cards, CARDS[Math.floor(Math.random() * CARDS.length)]])
+  const addCard = () => {
+    const randoUrl = IMG_URLS[Math.floor(Math.random() * IMG_URLS.length)]
+    const newCard = { id: nextId.toString(), imgSrc: randoUrl }
+    setCards([...cards.slice(0), newCard])
+    setNextId(nextId + 1)
+  }
 
   // attach a simple function to each card to remove the card from your hand
   for (let card of cards) {
-    card.handleClick = (id) => setCards(cards.filter((c, i) => i !== id))
+    card.handleClick = (id) => setCards(cards.filter(c => c.id !== id))
   }
 
   return (
