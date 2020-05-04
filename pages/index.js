e74226a0fe0a4a8ff149cff11b2747de42153b6c
brettgrigsby <gbrett@gmail.com>
2020-05-04 14:16:43 -0500

    add a set of cards and a button to add cards

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
-import Head from 'next/head'
+import { useState } from 'react'
 import { Hand } from 'handy'
 
-const cards =[
-  { image: 'image-url' },
-  { image: 'image-url' },
-  { image: 'image-url' },
+const CARDS = [
+  { imgSrc: './atst.jpg' },
+  { cardText: 'Card with text supplied instead of an image' },
+  { imgSrc: './f11d.jpg' },
+  {},
+  { imgSrc: './grievous.jpg' },
+  { imgSrc: './phasma.jpg' },
+  { imgSrc: './presence.jpg' },
+  { imgSrc: './tfighter.jpg' },
+  { imgSrc: './trooper.jpg' },
+  { imgSrc: './veers.jpg' },
 ]
 
 export default function Home() {
+  const [cards, setCards] = useState(CARDS.slice(0,4))
+
+  const addCard = () => setCards([ ...cards, CARDS[Math.floor(Math.random() * CARDS.length)]])
+
   return (
     <div className="container">
+      <button onClick={addCard}>Add Card</button>
       <Hand cards={cards} />
     </div>
   )
